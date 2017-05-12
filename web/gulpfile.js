e95b5c8e8aa59491003a53744e93d004f6455126
junjun <xyq153596@163.com>
2017-05-12 18:08:47 +0800

    修改

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,20 +1,45 @@
-let gulp = require("gulp");
 let browserSync = require("browser-sync").create();
+let gulp = require("gulp");
 let sass = require("gulp-sass");
+let watch = require("gulp-watch");
+let concat = require("gulp-concat");
+let minicss = require("gulp-clean-css");
+let rename = require("gulp-rename");
+let fileInclude = require('gulp-file-include');
 let reload = browserSync.reload;
-let fileInclude  = require('gulp-file-include');
 
-gulp.task("browser-sync",()=>{
+/*----调试-----*/
+gulp.task("init", ["sass","htmlInclude"], () => {
   browserSync.init({
-    server:{
-      baseDir:"./"//初始路径
+    server: {
+      baseDir: "dist/html" //初始路径
     }
   });
+  gulp.watch("resource/sass/**/*.scss",["sass"]);
+  gulp.watch("view/!(common)/*.html",["htmlInclude"]);
+  gulp.watch("dist/html/**/*.html").on("change",reload);
+})
+/*---合并html----*/
+gulp.task("htmlInclude", () => {
+  gulp.src("view/!(common)/*.html")
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('dist/html'));
 });
 
-gulp.task("htmlInclude",()=>{
-  gulp.src("view/**/*.html").pipe(fileInclude({
-    prefix:'@@',
-    basepath:'@file'
-  })).pipe(gulp.dest('dist'));
-});
+/*-----sass处理-----*/
+gulp.task("sass", () => {
+  gulp.src("resource/sass/**/*.scss")
+    .pipe(sass()) //转化
+    .pipe(minicss()) //压缩
+    .pipe(rename({
+      suffix: ".min"
+    })) //重命名
+    .pipe(gulp.dest("dist/css"))
+})
+
+
+
+gulp.task("build", ["htmlInclude", "sass"]);
